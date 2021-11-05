a3658a285390c53aabecc23816cc05bd975de33d
sed <sedademirgf@gmail.com>
2021-11-05 19:53:20 +0000

    animate logo

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,28 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import "./Header.css";
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rotating: false,
+    };
+    this.toggleRotate = this.toggleRotate.bind(this);
+  }
+  toggleRotate(e) {
+    this.setState((st) => {
+      return { rotating: !st.rotating };
+    });
+    console.log("start");
+  }
+
+  rotateEnd() {}
   render() {
+    const rotating = this.state.rotating;
     return (
-      <header>
+      <header onMouseEnter={this.toggleRotate} onMouseLeave={this.toggleRotate}>
         <div className="Logo">
           <div>
-            <i className="fab fa-stripe-s"></i>
+            <i className={`fab fa-stripe-s ${rotating ? "rotating" : ""}`}></i>
           </div>
           <span>eda.</span>
         </div>
